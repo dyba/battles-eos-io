0487bae7ee5fbc25b3db7be2c697b506636bf5fd
Daniel Dyba <daniel.dyba@sagewise.io>
2018-11-02 08:47:03 -0700

    Updates cleos alias

--- cleos-alias.sh
+++ cleos-alias.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-alias cleos='docker exec -it nodeos /opt/eosio/bin/cleos --url http://127.0.0.1:8888 --wallet-url http://172.18.0.3:9876'
+alias cleos='docker exec -it nodeos /opt/eosio/bin/cleos --url http://127.0.0.1:8888 --wallet-url http://172.18.0.2:9876'
 
