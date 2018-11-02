55de53f9ff10999f2e5e87563c5865f36f853041
Daniel Dyba <daniel.dyba@sagewise.io>
2018-11-02 08:47:37 -0700

    Updates script to upload contracts

--- upload-contracts.sh
+++ upload-contracts.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-cleos set contract <account_name> cardgame.wasm cardgame.abi
+account_name=""
+dir=""
+
+while getopts ":n:d:" opt; do
+    case ${opt} in
+        n)
+            account_name=$OPTARG
+            ;;
+        d)
+            dir=$OPTARG
+            ;;
+    esac
+done
+
+
+[ -z "$account_name" ] && echo "name must be provided" && exit 1
+[ -z "$dir" ] && echo "dir must be provided" && exit 1
+
+source cleos-alias.sh
+
+cleos set contract $account_name $dir cardgame.wasm cardgame.abi
