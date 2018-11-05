5237e0939d780921e1dba1e916786ed3f655c215
Daniel Dyba <daniel.dyba@gmail.com>
2018-11-04 17:34:43 -0800

    Fixes indentation

--- contracts/cardgame.hpp
+++ contracts/cardgame.hpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-class[[eosio::contract]] cardgame : public eosio::contract {
+class [[eosio::contract]] cardgame : public eosio::contract {
 
-public:
+ public:
   cardgame(eosio::name receiver, eosio::name code,
            eosio::datastream<const char *> ds)
       : contract(receiver, code, ds), _users(receiver, 0){};
 
   [[eosio::action]] void login(eosio::name username);
 
-private:
-  struct[[eosio::table]] user {
+ private:
+  struct [[eosio::table]] user {
     eosio::name key;
     uint16_t win_count = 0;
     uint16_t lost_count = 0;
