de1cc06e93b5457d9b7a5d7b5fa0a0a408988f2c
Daniel Dyba <daniel.dyba@gmail.com>
2018-11-04 20:48:22 -0800

    Drops quotes

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -68,12 +68,12 @@ class ApiService {
     try {
       const rpc = new JsonRpc(process.env.REACT_APP_EOS_HTTP_ENDPOINT);
       const result = await rpc.get_table_rows({
-        "json": true,
-        "code": process.env.REACT_APP_EOS_CONTRACT_NAME,
-        "scope": process.env.REACT_APP_EOS_CONTRACT_NAME,
-        "table": "users",
-        "limit": 1,
-        "lower_bound": username
+        json: true,
+        code: process.env.REACT_APP_EOS_CONTRACT_NAME,
+        scope: process.env.REACT_APP_EOS_CONTRACT_NAME,
+        table: "users",
+        limit: 1,
+        lower_bound: username
       });
       return result.rows[0];
     } catch (err) {
